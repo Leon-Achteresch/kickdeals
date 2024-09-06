fd214c9017316f439f92622df854ce1c59b94f9f
DESKTOP-5HUBRHC\Leon-PC <leon.achteresch@gmail.com>
2024-09-06 19:37:15 +0200

    animationen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
+"use client";
 import { TrendingUp } from "lucide-react";
 import Hero from "../components/hero/hero";
 import SearchSection from "../components/search-section/search-section";
 import FeaturedDealsCarousel from "../components/featured-deals-caroussel/featured-deals-caroussel";
 import NewsletterSection from "../components/newsletter-section/newsletter-section";
+import { motion } from "framer-motion";
 
-export default async function Index() {
+export default function Index() {
   return (
-    <main className=" flex flex-col gap-10 px-4 max-w-6xl mx-auto w-full ">
-      <Hero />
-      <SearchSection />
+    <main className="flex flex-col gap-10 px-4 max-w-6xl mx-auto w-full">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <Hero />
+      </motion.div>
 
-      <section>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <SearchSection />
+      </motion.div>
+
+      <motion.section
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
         <div className="text-2xl font-semibold mb-6 flex items-center justify-between w-full">
           <div className="flex items-center flex-row">
             <TrendingUp className="mr-2 h-6 w-6" /> Featured Deals
@@ -20,12 +39,23 @@ export default async function Index() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8 }}
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+        >
           <FeaturedDealsCarousel />
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
 
-      <NewsletterSection />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.4 }}
+      >
+        <NewsletterSection />
+      </motion.div>
     </main>
   );
 }
